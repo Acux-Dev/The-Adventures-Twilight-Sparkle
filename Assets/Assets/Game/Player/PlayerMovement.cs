facf1064cdcbb8e4a4a9697600205732020033ad
Acux-dev <juansup123@gmail.com>
2024-11-06 10:39:43 -0500

    Sprite: pony
    changed character

--- Assets/Assets/Game/Player/PlayerMovement.cs
+++ Assets/Assets/Game/Player/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public Vector2 direction;
 
+    [SerializeField]
     Rigidbody2D rigidbody;
 
     private void Start()
